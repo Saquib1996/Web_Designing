e7e7347f2ebc83bbd281d317b3a08d8822b76903
Saquib1996 <mdabusaquibhassan23@gmail.com>
2023-02-03 13:49:08 +0530

    Express

---
+++ Express/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const port = 3000;
+app.use('/static', express.static('static'));
+// Set the template engine as pug
+app.set('view engine', 'pug');
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+// Our pug demo endpoint
+
+app.get('/demo', (req, res) => {
+    res.status(200).render('demo', { title: 'Hey Saquib', message: 'Hello there and thanks for telling me how to use pug !' })
+})
+
+app.get('/', (req, res) => {
+    res.send('This is home page of my first express app with Harry');
+});
+app.get('/about', (req, res) => {
+    res.send('This is about page my first express app with Harry');
+});
+app.post('/about', (req, res) => {
+    res.send('This is a post of my first express app with Harry');
+});
+app.get('/this', (req, res) => {
+    res.status(404).send('This page is not found');
+});
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`);
+})
